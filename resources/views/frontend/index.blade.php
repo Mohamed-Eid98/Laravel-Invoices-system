0efb4dd841a8d052eeb9c02544c0cad07ad68cee
Mohamed Eid Saad <m.eid.saad403@gmail.com>
2022-09-24 10:11:09 -0700

    first commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-12">
+            iam index
+        </div>
+    </div>
+@endsection
